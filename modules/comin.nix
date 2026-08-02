4fabf362f5ee61857e168a1a4020ae0b686c6930
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2026-08-02 13:13:00 +0200

    Add access token path

--- modules/comin.nix
+++ modules/comin.nix
@@ -15,7 +15,8 @@
         name = "origin";
         url = "git@github.com:s3n-w6i/nix-conf";
         branches.main.name = "master";
-        auth.ssh_deploy_key_path = "/home/admin/.ssh/id_ed25519";
+        # auth.ssh_deploy_key_path = "/home/admin/.ssh/id_ed25519"; # this does not yet seem functional, so using access_token_path instead
+        auth.access_token_path = "/home/admin/secrets/github_access_token";
       }
     ];
     gpgPublicKeyPaths = [
